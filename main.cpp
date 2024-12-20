d49b0cd6877c9e20c96d1d25d941501cd27fec90
dalalit <154465141+dalalit@users.noreply.github.com>
2024-12-20 20:04:45 +0000

    dfsfs

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
 
+int main() {
+    std::cout << "Rock Paper Scissors!"<< std::endl;
+    char user_choice;
+    std::cout << "Enter choice(r, p, s): "; std::cin >> user_choice;
+    std::fstream seed_file("main.txt");
+    char seed;
+    seed_file >> seed;
+    seed++;
+    std::cout << seed;
+    
+    
+}
